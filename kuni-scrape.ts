823c582b22d68183740450e9ce9563cea3d0b603
Julian Lo <julian.lo@gmail.com>
2020-09-28 23:29:54 -0700

    Add B&H

--- kuni-scrape.ts
+++ kuni-scrape.ts
@@ -10,7 +10,10 @@ const URLS: string[] = [
     'https://www.evga.com/products/product.aspx?pn=10G-P5-3883-KR',
     'https://www.evga.com/products/product.aspx?pn=10G-P5-3885-KR',
     'https://www.newegg.com/p/pl?d=RTX+3080&N=100007709%20601357282&isdeptsrh=1',
+    'https://www.bhphotovideo.com/c/search?q=rtx%203080&filters=fct_category%3Agraphic_cards_6567',
 ];
+
+const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36';
 
 type StockResult = {
     available: boolean;
@@ -25,17 +28,56 @@ const evgaGetProductStock = (dom: JSDOM): StockResult => ({
 
 const neweggGetProductStock = (dom: JSDOM): StockResult => {
     // Assumes the link is a search results page
-    let queryItem = '.item-container .item-info';
-    let promoTexts = dom.window.document.querySelectorAll(`${queryItem} .item-promo`);
-    let infos = dom.window.document.querySelectorAll(`${queryItem} .item-title`);
-    var result: StockResult = {
+    const queryItem = '.item-container .item-info';
+    const promoTexts = dom.window.document.querySelectorAll(`${queryItem} .item-promo`);
+    const infos = dom.window.document.querySelectorAll(`${queryItem} .item-title`);
+    let result: StockResult = {
         available: false,
         product: 'NewEgg',
     };
     promoTexts.forEach((node, index) => {
-        let available = !node.innerHTML.includes('OUT OF STOCK');
-        let url = infos.item(index).href;
-        let product = infos.item(index).innerHTML;
+        const available = !node.innerHTML.includes('OUT OF STOCK');
+        const url = infos.item(index).href;
+        const product = infos.item(index).innerHTML;
+        log(`Product: ${product}\nLink: ${url}\nAvailable: ${available}\n`);
+        if (available) {
+            result = {
+                available: true,
+                product,
+                url,
+            };
+        }
+    });
+    return result;
+};
+
+const bestbuyGetProductStock = (dom: JSDOM): StockResult => {
+    console.log('best buy');
+    const itemLinks = dom.window.document.querySelectorAll('.sku-item .sku-header a');
+    const addToCardButtons = dom.window.document.querySelectorAll('.sku-item .fulfillment-add-to-cart-button');
+
+    itemLinks.forEach((node, index) => {
+        console.log(node);
+    });
+    return {
+        available: false,
+        product: 'fake',
+    };
+};
+
+const bhGetProductStock = (dom: JSDOM): StockResult => {
+    const productNames = dom.window.document.querySelectorAll('div[class^=productInner] h3 a span');
+    const links = dom.window.document.querySelectorAll('div[class^=productInner] h3 a');
+    const conversions = dom.window.document.querySelectorAll('div[class^=productInner] div[class^=conversion]');
+
+    let result: StockResult = {
+        available: false,
+        product: 'B&H',
+    };
+    productNames.forEach((node, index) => {
+        const product = node.innerHTML;
+        const available = !!conversions[index].querySelector('button[class^=atcBtn]');
+        const url = `https://www.bhphotovideo.com${links[index].href}`;
         log(`Product: ${product}\nLink: ${url}\nAvailable: ${available}\n`);
         if (available) {
             result = {
@@ -50,7 +92,10 @@ const neweggGetProductStock = (dom: JSDOM): StockResult => {
 
 const getProductStock = async (url: string): Promise<StockResult | null> => {
     try {
-        const response = await fetch(url);
+        const response = await fetch(url, {
+            'Accept': 'text/html',
+            'User-Agent': userAgent,
+        });
         const text = await response.text();
         const dom = await new JSDOM(text);
         if (url.startsWith('https://www.evga.com')) {
@@ -58,6 +103,12 @@ const getProductStock = async (url: string): Promise<StockResult | null> => {
         } 
         if (url.startsWith('https://www.newegg.com')) {
             return neweggGetProductStock(dom);
+        }
+        if (url.startsWith('https://www.bestbuy.com')) {
+            return bestbuyGetProductStock(dom);
+        }
+        if (url.startsWith('https://www.bhphotovideo.com')) {
+            return bhGetProductStock(dom);
         }
     } catch (e) {
         log(`Failed checking url: ${url}\nError: ${e}`);
@@ -68,7 +119,7 @@ const getProductStock = async (url: string): Promise<StockResult | null> => {
 
 const checkUrl = async (queryUrl: string): Promise<void> => {
     const result = await getProductStock(queryUrl);
-    var text: string;
+    let text: string;
     if (result) {
         const { available, product, url } = result;
         text = `Product: ${product}\nAvailable: ${available}`;
@@ -101,7 +152,7 @@ const log = (text: string) => {
 
 
 const main = async () => {
-    let url = process.argv[2];
+    const url = process.argv[2];
     if (url === 'push') {
         console.log('Sending test push');
         notifier.notify({
